58136df4b147cc3391086b32d60f689458a8ac8a
Pavel Kozlov <cozlov.pavel@mail.ru>
2022-11-20 16:11:57 +0300

    Corrections commit

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        int account = 100; // Счёт
-        int a = 1100; // Пополнения на счёт
-        int bonus = a / 100; // Бонусные рубли
-        if (a > 1000){
+        int account = 10; // Счёт
+        int adding = 1100; // Пополнения на счёт
+        int bonus = adding / 100; // Бонусные рубли
+        if (adding > 1000){
             bonus = bonus;
 
         } else {
             bonus = 0;
         }
-        int adding = a + account + bonus; // Баланс счёта после пополнения
+        int addingAccount = adding + account + bonus; // Баланс счёта после пополнения
 
-        System.out.println("На вашем счету:" + adding);
+        System.out.println("На вашем счету:" + addingAccount);
         System.out.println("Из них бонусных:" + bonus);
     }
 
